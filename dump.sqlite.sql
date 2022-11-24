7c0119d80f46460ecf12fe3e9dd90156e5cb32a8
amank2608 <118986716+amank2608@users.noreply.github.com>
2022-11-24 19:24:07 +0530

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE countries ( 
+COUNTRY_ID varchar(2) NOT NULL,
+COUNTRY_NAME varchar(40) NOT NULL,
+REGION_ID decimal(10,0) NOT NULL
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
